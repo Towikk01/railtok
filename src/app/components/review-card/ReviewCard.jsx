ff709f68900f87c5273f32dcbf10ff5193279c79
Towikk01 <anton.masuytkin@gmail.com>
2024-04-02 20:22:09 +0300

    feat(root):fixed app 5

--- src/app/components/review-card/ReviewCard.jsx
+++ src/app/components/review-card/ReviewCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ReviewCard = ({name, date, reviewText}) => {
     return (
-        <figure className="border border-black min-h-[200px] h-full text-black w-full rounded-[5px] bg-[#666] bg-opacity-20 shadow-md">
+        <figure className="border border-black min-h-[200px] h-fit text-black w-full rounded-[5px] bg-[#666] bg-opacity-20 shadow-md">
             <figcaption className="flex flex-col">
             <div className="flex flex-row justify-between border-b border-black rounded-tl h-full rounded-tr px-5 py-2 items-center">
                     <span className="font-bold text-xl text-white">{name}</span>
