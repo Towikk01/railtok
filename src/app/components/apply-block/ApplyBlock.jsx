296d9fd6c3781669167596fb0395cc31c2708aba
Towikk01 <anton.masuytkin@gmail.com>
2024-04-19 16:49:56 +0300

    fixed 32

--- src/app/components/apply-block/ApplyBlock.jsx
+++ src/app/components/apply-block/ApplyBlock.jsx
@@ -1,16 +1,15 @@
 // components/DynamicBlock.jsx
-const DynamicBlock = ({ title, description, isLast = true }) => {
+const DynamicBlock = ({ title, description, isLast = false }) => {
 	return (
 		<div className={`flex ${title ? 'md:flex-col md:gap-9' : 'md:flex-col md:gap-2'} items-start flex-row   last:mb-0 gap-[22px] w-fit`}>
-			<div className={`flex flex-col ${title ? 'md:flex-col' : 'md:flex-row'} items-center gap-[1.15rem] md:gap-5 md:w-full`}>
+			<div className={'flex flex-col items-center gap-[1.15rem] md:flex-row md:gap-5 md:w-full'}>
 				<div
 					className="min-w-[30px] h-[30px] bg-amber-100 animate-pulse bg-opacity-20 rounded-full flex items-center justify-center">
 					<div className="w-[15px] h-[15px] bg-white rounded-full"></div>
 				</div>
-				{isLast && (
-					<div
-						className={'w-0.5 h-24 bg-transparent border-l-2 border-dashed border-gray-300 md:border-l-0 md:border-b md:w-full md:h-0.5'}></div>
-				)}
+
+				<div
+					className={`w-0.5 h-24 bg-transparent border-l-2 border-dashed border-gray-300 md:border-l-0 md:border-b md:w-full md:h-0.5 ${isLast ? 'h-48': 'h-24'}`}></div>
 			</div>
 			<div className={'flex flex-col gap-4'}>
 				{title && (
@@ -18,7 +17,7 @@ const DynamicBlock = ({ title, description, isLast = true }) => {
 						{title}
 					</h3>
 				)}
-				<p className={`${title ? 'text-[21px]' : 'text-[26px]'} md:text-[20px] text-white  md:leading-[26px] text-white/95 font-normal leading-[22px]  md:mb-0 md:mr-[30px] lg:mr-0 lg:max-w-[306px] md:max-w-[163px]`}>
+				<p className={`${title ? 'text-[21px]' : 'text-[20px] mt-12 md:mt-0'} md:text-[20px] text-white  md:leading-[26px] text-white/95 font-normal leading-[22px]  md:mb-0 md:mr-[30px] lg:mr-0 lg:max-w-[306px] md:max-w-[163px]`}>
 					{description}
 				</p>
 			</div>
--- src/app/sections/about/About.jsx
+++ src/app/sections/about/About.jsx
@@ -9,14 +9,14 @@ import ApplyBlock from '@/app/components/apply-block/ApplyBlock';
 const AboutSection = () => {
 	const { language, setLanguage } = useLanguage();
 	const currentTranslations = translations[language];
-	const data = [currentTranslations.about.p1, currentTranslations.about.p2, currentTranslations.about.p3, currentTranslations.about.p4];
+	const data = [{item: currentTranslations.about.p1}, {item:currentTranslations.about.p2}, {item:currentTranslations.about.p3}, {item:currentTranslations.about.p4, isLast: true}];
 	return (
 		<section id="about" className="flex md:flex-row w-full px-5 py-10 border-black border-b">
 			<div className="w-full flex flex-col md:flex-row items-center justify-between gap-8">
 				<div className="text-white md:w-2/4 w-full flex flex-col gap-5 items-center md:items-start">
 					<h4 className="text-[50px] relative text-center md:after:hidden text-white after:absolute w-fit after:w-full md:after:w-3/4 after:h-0.5 after:bg-black after:content-[''] after:-bottom-0 after:left-0">{currentTranslations.about.title}</h4>
 					{data.map((item, index) => (
-						<ApplyBlock key={index} description={item} className="text-[20px] text-white leading-8 text-center"/>
+						<ApplyBlock key={index} description={item.item} isLast={item.isLast} className="text-[20px] text-white leading-8 text-center"/>
 					))}
 				</div>
 				<div className="flex flex-col gap-2 items-center">
