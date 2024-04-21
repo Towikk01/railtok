6cdd19572906f8ed961d681b6671b9b227e4afdb
Towikk01 <anton.masuytkin@gmail.com>
2024-04-21 15:50:35 +0300

    fixed 41

--- src/app/components/review-card/ReviewCard.jsx
+++ src/app/components/review-card/ReviewCard.jsx
@@ -6,7 +6,6 @@ const ReviewCard = ({name, date, reviewText}) => {
 			<figcaption className="flex flex-col">
 				<div className="flex flex-col sm:flex-row justify-between border-b border-black rounded-tl h-full rounded-tr px-5 py-2 items-center">
 					<span className="font-bold text-xl text-white">{name}</span>
-					<span className="font-bold text-sm text-black">{date}</span>
 				</div>
 				<p className="font-normal text-[#ADD9E9] h-full w-full py-3 px-5 text-[18px]">{reviewText}</p>
 			</figcaption>
