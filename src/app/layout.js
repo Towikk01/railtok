ccce7c9930aa5cb054d2b08667b8ac1b3c80d8ad
Towikk01 <anton.masuytkin@gmail.com>
2024-04-22 16:12:58 +0300

    fixed 42

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Fenix } from 'next/font/google';
 import './globals.css';
-import Head from 'next/head';
 import Header from '@/app/components/header/Header';
 import ParticlesBG from '@/app/components/particles/ParticlesBG';
 import Footer from '@/app/components/footer/Footer';
@@ -21,7 +20,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" style={{scrollBehavior:'smooth'}}>
-			<Script
+		<Script strategy="afterInteractive">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-P5BPT4DJ');
+		</Script>
+		<Script
 				id="fb-pixel"
 				strategy="afterInteractive"
 				dangerouslySetInnerHTML={{
